009d85598f95fd18f47461aad8acbf6b81e6ac73
Joan Gamell <gamell@gmail.com>
2016-06-01 22:42:15 -0700

    Add ping to google to check external connectivity
    Also adding bash shebang

--- vpnReconnect.sh
+++ vpnReconnect.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
 #Adapted from http://der-zyklop.de/blog/the-reconnection-of-vpn-profile-onsynology-nas-has-failed
 
-if echo `ifconfig ppp200` | grep -q "Link encap:Point-to-Point Protocol"
+echo `ifconfig ppp200` | grep -q "Link encap:Point-to-Point Protocol"
+vpn=$(echo $?)
+ping -c 3 google.com
+pingresult=$(echo $?)
+if [ "$vpn" -eq 0 -a "$pingresult" -eq 0 ]
 then
     echo "VPN is running"
 else
